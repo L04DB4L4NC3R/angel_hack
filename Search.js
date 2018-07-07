22f22ecb9834da1669a16d0925287e1527dc77d1
Arjun@1234 <arjunagarwal1998@gmail.com>
2018-07-07 15:23:59 +0530

    updated Search

--- Search.js
+++ Search.js
@@ -7,12 +7,13 @@ function getProperties(place){
 return new Promise((resolve,reject)=>{
 
 //fetching properties
-request('https://api.nestoria.in/api?encoding=json&pretty=1&action=search_listings&country=in&listing_type=buy&place_name="+place+"&number_of_results=5')
+request("https://api.nestoria.in/api?encoding=json&pretty=1&action=search_listings&country=in&listing_type=buy&place_name="+place+"&number_of_results=5")
 .then((res)=>{
   var data=JSON.parse(res.body);
+  console.log(data.response.listings);
   var arr=[];
   for(var prop of data.response.listings)
-  arr.push({name:prop.keywords,contact:"",email:"",location:{lat:prop.latitude,long:prop.longitude}});
+  arr.push({name:prop.keywords,contact:"",email:"",location:{lat:prop.latitude,long:prop.longitude},description:prop.summary,address:prop.title});
   resolve(arr);
   console.log(arr);
 })
@@ -20,5 +21,6 @@ request('https://api.nestoria.in/api?encoding=json&pretty=1&action=search_listin
 
 });
 }
+getProperties('gurgaon').then((res)=>console.log(res)).catch((err)=>console.log(err));
 
 module.exports=getProperties;
